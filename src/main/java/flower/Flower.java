c797e391a88ccabc5b439d9be50ed4dfa2833eb0
ivaniuk <ivaniuk.pn@ucu,edu,ua>
2023-10-26 23:03:47 +0300

    edited linter

--- src/main/java/flower/Flower.java
+++ src/main/java/flower/Flower.java
@@ -27,12 +27,12 @@ public class Flower {
         return color.toString();
     }
 
-    public void setPrice(double new_price) {
-        if (new_price < 0) {
+    public void setPrice(double newPrice) {
+        if (newPrice < 0) {
             price = 0;
         }
         else {
-            price = new_price;
+            price = newPrice;
         }
     }
 }
